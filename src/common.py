f90892971b3b83179699a0454a6186b37179f640
Luis Capelo <953118+luiscape@users.noreply.github.com>
2025-06-05 15:07:58 -0400

    Small updates to common (#6)

--- src/common.py
+++ src/common.py
@@ -5,14 +5,9 @@ PARAKEET_MODEL_NAME = "nvidia/parakeet-tdt-0.6b-v2"
 WHISPER_MODEL_NAME = "openai/whisper-large-v3-turbo"
 WHISPERX_MODEL_NAME = "large-v2"
 
-# App + volume
-APP_NAME = "audio-diarization-benchmarking-app"
-MODEL_CACHE_VOLUME_NAME = "audio-diarization-model-cache"
-DATASET_VOLUME_NAME = "audio-diarization-benchmarking-data"
-
-app = modal.App(APP_NAME)
-dataset_volume = modal.Volume.from_name(DATASET_VOLUME_NAME, create_if_missing=True)
-model_cache = modal.Volume.from_name(MODEL_CACHE_VOLUME_NAME, create_if_missing=True)
+app = modal.App("asr-benchmark")
+dataset_volume = modal.Volume.from_name("asr-benchmark-cache", create_if_missing=True)
+model_cache = modal.Volume.from_name("asr-benchmark-data", create_if_missing=True)
 
 GPUS = ["t4", "a10g", "h100"]
 
