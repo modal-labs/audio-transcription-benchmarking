4302b21161901c36a51e0b0c5f3c758cc5cf8877
Luis Capelo <luis@modal.com>
2025-06-05 16:30:25 -0400

    Benchmark whisper

--- src/benchmark_whisperx.py
+++ src/benchmark_whisperx.py
@@ -2,7 +2,7 @@ import time
 from pathlib import Path
 
 import modal
-from src.common import app, dataset_volume, model_cache, GPUS
+from src.common import app, dataset_volume, model_cache, GPUS, BenchmarkResult, MODEL_CACHE_PATH
 from src.utils import write_results
 
 MODEL_NAME = "large-v2"
@@ -16,11 +16,11 @@ whisperx_image = (
     .env(
         {
             "HF_HUB_ENABLE_HF_TRANSFER": "1",
-            "HF_HOME": "/cache",
+            "HF_HOME": MODEL_CACHE_PATH.as_posix(),
         }
     )
-    .run_commands(
-        "uv pip install --system evaluate==0.4.3 jiwer==3.1.0 librosa==0.11.0 hf_transfer faster-whisper whisperx torchaudio"
+    .pip_install(
+        "evaluate==0.4.3", "jiwer==3.1.0", "librosa==0.11.0", "hf_transfer==0.1.9", "faster-whisper==1.1.1", "whisperx==3.3.4", "torchaudio==2.7.1",
     )
     .apt_install("ffmpeg")
     .entrypoint([])
@@ -31,17 +31,20 @@ whisperx_image = (
 with whisperx_image.imports():
     import librosa
     import whisperx
+    import evaluate
 
 
 @app.cls(
     secrets=[modal.Secret.from_name("huggingface-secret")],
     volumes={
         "/data": dataset_volume,
-        "/cache": model_cache,
+        MODEL_CACHE_PATH.as_posix(): model_cache,
     },
     image=whisperx_image,
 )
 class WhisperX:
+    gpu: str = modal.parameter()
+
     @modal.enter()
     def load(self):
         import json
@@ -52,63 +55,59 @@ class WhisperX:
             self.metadata = json.load(f)
 
     @modal.method()
-    def run(self, file: str) -> tuple[str, str, float, float]:
-        import evaluate
+    def run(self, file: Path) -> BenchmarkResult:
 
-        file_path = Path(file)
-        filename = file_path.name
-
+        filename = file.name
         file_metadata = self.metadata.get(filename)
         if file_metadata is None:
-            return {
-                "model": MODEL_NAME.replace("/", "-"),
-                "filename": filename,
-                "expected_transcription": None,
-                "transcription": None,
-                "transcription_time": None,
-                "audio_duration": None,
-                "wer": None,
-                "gpu": self.gpu,
-            }
+            return BenchmarkResult(
+                model=MODEL_NAME.replace("/", "-"),
+                filename=filename,
+                expected_transcription=None,
+                transcription=None,
+                transcription_time=None,
+                audio_duration=None,
+                wer=None,
+                gpu=self.gpu,
+            )
 
         expected = file_metadata["transcription"]
 
-        y, sr = librosa.load(file_path, sr=None)
+        y, sr = librosa.load(file, sr=None)
         duration = len(y) / float(sr)
 
-        start_time = time.time()
-        audio = whisperx.load_audio(file_path, sr=16000)
+        start_time = time.perf_counter()
+        audio = whisperx.load_audio(file, sr=16000)
         result = self.model.transcribe(audio, batch_size=16)
-        transcription_time = time.time() - start_time
+        transcription_time = time.perf_counter() - start_time
 
         transcription = " ".join([s["text"] for s in result["segments"]])
 
         wer_model = evaluate.load("wer")
         wer = wer_model.compute(predictions=[transcription], references=[expected])
 
-        return {
-            "model": f"whisperx-{MODEL_NAME}",
-            "filename": filename,
-            "expected_transcription": expected,
-            "transcription": transcription,
-            "transcription_time": transcription_time,
-            "audio_duration": duration,
-            "wer": wer,
-            "gpu": self.gpu,
-        }
+        return BenchmarkResult(
+            model=f"whisperx-{MODEL_NAME}",
+            filename=filename,
+            expected_transcription=expected,
+            transcription=transcription,
+            transcription_time=transcription_time,
+            audio_duration=duration,
+            wer=wer,
+            gpu=self.gpu,
+        )
 
 
-@app.local_entrypoint()
+@app.function(
+    volumes={
+        MODEL_CACHE_PATH.as_posix(): dataset_volume,
+    }
+)
 def benchmark_whisperx():
     files = [
         str(Path("/data") / Path(f.path)) for f in dataset_volume.listdir("/processed")
     ]
-
     for gpu in GPUS:
-        whisperx = WhisperX.with_options(gpu=gpu)()
-        whisperx.gpu = gpu
-
+        whisperx = WhisperX.with_options(gpu=gpu)(gpu=gpu)
         results = list(whisperx.run.map(files))
-        results_path = write_results(results, f"whisperx-{MODEL_NAME}")
-        with dataset_volume.batch_upload() as batch:
-            batch.put_file(results_path, f"/results/{results_path}")
+        write_results(results, f"whisperx-{MODEL_NAME}")
